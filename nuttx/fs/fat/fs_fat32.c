75b6e951fbc840516c64b63a6ed6fd726e8ccbb5
Petteri Aimonen <petteri.aimonen@optofidelity.com>
2013-02-01 15:42:53 +0200

    Fix "fs undefined" error when using fat_io_free.

--- nuttx/fs/fat/fs_fat32.c
+++ nuttx/fs/fat/fs_fat32.c
@@ -378,6 +378,7 @@ static int fat_close(FAR struct file *filep)
 {
   struct inode         *inode;
   struct fat_file_s    *ff;
+  struct fat_mountpt_s *fs;
   int                   ret = OK;
 
   /* Sanity checks */
@@ -388,6 +389,7 @@ static int fat_close(FAR struct file *filep)
 
   ff    = filep->f_priv;
   inode = filep->f_inode;
+  fs    = inode->i_private;
 
   /* Do not check if the mount is healthy.  We must support closing of
    * the file even when there is healthy mount.
@@ -405,6 +407,7 @@ static int fat_close(FAR struct file *filep)
 
   if (ff->ff_buffer)
     {
+      (void)fs; /* Unused if fat_io_free == free(). */
       fat_io_free(ff->ff_buffer, fs->fs_hwsectorsize);
     }
 
